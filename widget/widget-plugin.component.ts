74cacdf337277cf4e345b244b909985bc296cb50
Chanaka Rajapsksha <rw.chanaka@gmail.com>
2026-02-02 13:28:19 +0530

    fix all updated task without icon adding

---
+++ widget/widget-plugin.component.ts
@@ -0,0 +1,16 @@
+// widget/widget-plugin.component.ts
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-widget-plugin',
+  templateUrl: './widget-plugin.component.html',
+  styleUrls: ['./widget-plugin.component.css']
+})
+export class WidgetPluginComponent implements OnInit {
+  
+  constructor() {}
+
+  ngOnInit(): void {
+    // Widget initialization
+  }
+}
